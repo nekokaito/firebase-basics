b29e200e4c6ef4b2edcff8cb8b6218c52e1884ee
nekokaito <siddiqsazzad001@gmail.com>
2024-03-30 00:03:38 +0600

    Created Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Home from './components/Home';
+import Page from './components/Page';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children : [
+      {
+        path:"/",
+        element: <Page></Page>
+      }
+    ]
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
