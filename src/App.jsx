f77d90e9b7e891711911e047f230487abb8e8e1e
nekokaito <siddiqsazzad001@gmail.com>
2024-03-29 23:40:43 +0600

    created simple firebase basic repo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <>
+       
+       {/* Remove From Here */}
+
+        <div className="flex gap-3 mt-3">
+        <img className=" w-[100px]" src="https://web.programming-hero.com/home/level2Home/images/logo.svg" alt="" />
+        <div>
+        <h1 className="text-3xl">ViteTemplate, Tailwind, daisyUI, Prop-types and ReactIcons  <br></br>Installed Successfully</h1>
+            <p className="mt-3">Happy Coding!</p>
+        </div>
+        </div>
+            
+         {/* Remove From Here */}
+
+        </>
+    );
+}
+
+export default App;
