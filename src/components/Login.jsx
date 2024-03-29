745b0c85d34d298daa5301fe60997146f8adb045
nekokaito <siddiqsazzad001@gmail.com>
2024-03-30 01:36:04 +0600

    Created Sign in and Sign Out Method

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,14 @@
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../firebase/firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
+
 import { useState } from "react";
 import User from "./User";
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    const [user, setUser] = useState([]);
+    const [user, setUser] = useState(null);
 
    const loginFromGoogle = () => {
           signInWithPopup(auth, provider)
@@ -19,12 +20,30 @@ const Login = () => {
           })
           .catch(console.log('error'))
    } 
+
+   const singOutFromGoogle = () => {
+    const auth = getAuth();
+    signOut(auth).then(result => {
+        console.log(result)
+        setUser(null);
+      
+    }).catch((error) => {
+      console.log(error)
+    });
+   }
     return (
         <div className="flex flex-col justify-center items-center gap-5">
-            <button onClick={loginFromGoogle} className="btn">Google Login</button>  
-          <div className="flex p-10 flex-col justify-center items-center my-10">
-               <User user={user}></User>
-          </div>
+            {
+                user? <button onClick={singOutFromGoogle} className="btn">Sign Out</button> : <button onClick={loginFromGoogle} className="btn">Google Login</button>
+            }
+              
+            
+            
+            
+            {user && <div className="flex p-10 flex-col justify-center items-center my-10">
+             <User user={user}></User>
+       </div>}
+         
         </div>
     );
 };
